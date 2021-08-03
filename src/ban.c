70cc8065f1c71fce8745b0e9826128de2b925aa4
Lajule <julien.rouzieres@mac.com>
2021-08-04 00:25:55 +0200

    bytes

--- src/ban.c
+++ src/ban.c
@@ -7,12 +7,13 @@
 #endif
 
 #include <stdio.h>
+#include <string.h>
 #include "ban.h"
 
 static void get_hostname(char *, size_t);
-static const char *get_os();
-static long get_cores();
-static void get_mem();
+static void get_os(char *, size_t);
+static void get_cores(long *);
+static void get_mem(struct mem_s *);
 
 void ban(const char *name) {
 	#ifdef NDEBUG
@@ -24,58 +25,51 @@ void ban(const char *name) {
 	char hostname[150] = {'\0'};
 	get_hostname(hostname, 150);
 
-	const char *os = get_os();
+	char os[150] = {'\0'};
+	get_os(os, 150);
 
-	long cores = get_cores();
+	long cores = 0;
+	get_cores(&cores);
 
-	get_mem();
+	struct mem_s mem = {.available=0, .total=0};
+	get_mem(&mem);
 
-	printf("%s %s %s %.ld\n", name, hostname, os, cores);
+	printf("%s %s %s %.ld %.ldB/%.ldB\n", name, hostname, os, cores, mem.available, mem.total);
 }
 
 static void get_hostname(char *hostname, size_t length) {
 	#ifdef WIN32
 	DWORD size = length;
-	GetComputerName(hostname, &size)
+	GetComputerName(hostname, &size);
 	#else
 	gethostname(hostname, length);
 	#endif
 }
 
-static const char *get_os() {
-	#ifdef _WIN32
-	return "Windows 32-bit";
-	#elif _WIN64
-	return "Windows 64-bit";
-	#elif __APPLE__ || __MACH__
-	return "Mac OSX";
-	#elif __linux__
-	return "Linux";
-	#elif __FreeBSD__
-	return "FreeBSD";
-	#elif __unix || __unix__
-	return "Unix";
-	#else
-	return "Other";
-	#endif
+static void get_os(char *os, size_t length) {
+	strncpy(os, OS, length);
 }
 
-static long get_cores() {
+static void get_cores(long *cores) {
 	#ifdef WIN32
 	SYSTEM_INFO info = {.dwNumberOfProcessors=0};
 	GetSystemInfo(&info);
-	return info.dwNumberOfProcessors
+	*cores = info.dwNumberOfProcessors;
 	#else
-	return sysconf(_SC_NPROCESSORS_ONLN);
+	*cores = sysconf(_SC_NPROCESSORS_ONLN);
 	#endif
 }
 
-static void get_mem() {
+static void get_mem(struct mem_s *mem) {
 	#ifdef WIN32
-	MEMORYSTATUS status;
-	GlobalMemoryStatus(&status)
+	MEMORYSTATUS status = {.dwTotalPhys=0, .dwAvailPhys=0};
+	GlobalMemoryStatus(&status);
+	mem->available = status.dwAvailPhys;
+	mem->total = status.dwTotalPhys;
 	#else
 	struct sysinfo info;
 	sysinfo(&info);
+	mem->available = info.freeram * info.mem_unit;
+	mem->total = info.totalram * info.mem_unit;
 	#endif
 }
--- src/ban.h
+++ src/ban.h
@@ -6,4 +6,25 @@
   #define ban_EXPORT
 #endif
 
+#ifdef _WIN32
+	#define OS "Windows 32-bit"
+#elif _WIN64
+	#define OS "Windows 64-bit"
+#elif __APPLE__ || __MACH__
+	#define OS "Mac OSX"
+#elif __linux__
+	#define OS "Linux"
+#elif __FreeBSD__
+	#define OS "FreeBSD"
+#elif __unix || __unix__
+	#define OS "Unix"
+#else
+	#define OS "Other"
+#endif
+
+struct mem_s {
+  long available;
+  long total;
+};
+
 ban_EXPORT void ban(const char *);
